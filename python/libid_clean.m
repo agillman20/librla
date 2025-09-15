9ebd8b6f6e01a2e360ad94b04ce9b9e4f8747672
Zydrunas Gimbutas <zydrunas.gimbutas@gmail.com>
2025-09-14 20:04:13 -0600

    Sync files

--- python/libid_clean.m
+++ python/libid_clean.m
@@ -104,7 +104,6 @@ classdef libid
             A_proj = Q_basis' * A;
             [U_proj,S_proj,V_proj] = svd(A_proj, 'econ');
             k = sum(abs(diag(S_proj)) >= rtol*norm(A_proj,'fro'));
-
             Uk = Q_basis * U_proj(:,1:k);
             sk = diag(S_proj(1:k,1:k));
             Vk = V_proj(:,1:k)';
